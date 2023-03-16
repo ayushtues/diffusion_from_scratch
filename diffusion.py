Fix posterior variance bug

diffusion.py
@@ -18,8 +18,8 @@ class Diffusion(nn.Module):
         bilinear=False,
     ):
         super(Diffusion, self).__init__()
-        # self.model = UNet(n_channels, n_classes, bilinear=False)
-        self.model = SimpleUnet()
+        self.model = UNet(n_channels, n_classes, bilinear=False)
+        # self.model = SimpleUnet()
         # self.model = SimpleMLP()
         # self.model = SimpleMLP2()
         self.sqrt_alpha_hat_ts = sqrt_alpha_hat_ts
@@ -57,8 +57,8 @@ class Diffusion(nn.Module):
         # x = torch.randn([1, 2, 1, 1], device=device)
         x_returned = []
         for i in reversed(range(1000)):
-            # t = get_position_embeddings(i, device).unsqueeze(0)
-            t = torch.ones([1], device=device)*i
+            t = get_position_embeddings(i, device).unsqueeze(0)
+            # t = torch.ones([1], device=device)*i
             # print("x1:", x.shape)
             eps_pred = self.model(x, t)
             # print_stats(x, f"eps_pred_{i}")

models.py
@@ -32,7 +32,7 @@ def positionalencoding1d(d_model, length):
 
 
 def get_position_embeddings(t, device):
-    x = positionalencoding1d(512, 1000).to(device)
+    x = positionalencoding1d(32, 1000).to(device)
     emb = x[t]
     return emb
 
@@ -191,7 +191,7 @@ class UNet(nn.Module):
         self.up4 = Up(128, 64, bilinear)
         self.outc = OutConv(64, n_classes)
 
-        input_size = [512, 64, 128, 256, 512, 1024, 512, 256, 128, 64]
+        input_size = [32, 64, 128, 256, 512, 1024, 512, 256, 128, 64]
         self.linears = nn.ModuleList(
             [
                 nn.Linear(input_size[0], input_size[i + 1])

train.py
@@ -62,8 +62,8 @@ def train_one_epoch(epoch_index, batches, tb_writer, run_path, save_freq=2000):
         x = x * 2 - 1
         t = t.to(device)
         t = t.squeeze(-1)
-        # t_embed = get_position_embeddings(t, device)
-        t_embed = t
+        t_embed = get_position_embeddings(t, device)
+        # t_embed = t
         eps = torch.randn_like(x)
 
         # Zero your gradients for every batch!

utils.py
@@ -15,7 +15,7 @@ def get_values(device):
   sqrt_alpha_ts = torch.sqrt(alpha_ts)
   sqrt_alpha_hat_ts = torch.sqrt(alpha_hat_ts)
   sqrt_alpha_hat_ts_2 = torch.sqrt(1-alpha_hat_ts)
-  post_std = torch.sqrt((alpha_hat_ts_prev/alpha_hat_ts)*beta_ts)
+  post_std = torch.sqrt(((1-alpha_hat_ts_prev)/(1-alpha_hat_ts))*beta_ts)
 
   # prev_sqrt_alpha_hat_ts = sqrt_alpha_hat_ts[:-1] # sqrt(alpha_hat_t-1)
   # prev_alpha_hat_ts = alpha_hat_ts[:-1] # alpha_hat_t-1